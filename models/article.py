e4e7cf6abd7e7eca1e6e58778eaf329db7c8715d
Sam Johnson <sam@3ev.com>
2018-02-19 22:16:25 +0000

    Split project into two

--- models/article.py
+++
@@ -1,9 +0,0 @@
-from .database_tools import Base
-from sqlalchemy import Column, Integer, String, Float, Text
-
-class Article(Base):
-    __tablename__ = 'articles'
-    id = Column(Integer, primary_key=True)
-    source_url = Column(Text)
-    content = Column(Text)
-    avg_tone = Column(Float)
