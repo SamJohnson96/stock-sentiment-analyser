cf21c8cfbbb1fa2dd72b4bff629f4167d53adc68
Sam Johnson <sam@3ev.com>
2018-02-28 16:53:41 +0000

    create new lambda trigger

---
+++ stock_sentiment_analyser/support_vector_machine.py
@@ -0,0 +1,62 @@
+import imp
+import requests
+import json
+import sys
+import boto3
+import botocore
+import time
+from datetime import datetime
+
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        if 'NewImage' in record['dynamodb']:
+            article_content = record['dynamodb']['NewImage']['article_content']['S']
+            article_id = record['dynamodb']['NewImage']['article_id']['N']
+        else:
+            print('No article to scrape')
+            return;
+    print('---- Parsing article ----')
+    classification = classify_new_article(article_content);
+
+    if classification is not None:
+        print('---- Inserting/Updating row into parsed_articles ----')
+        #Insert into dynamoDb
+        insert_row(article_id,article_content,classification);
+        print('---- Done ----')
+
+def classify_new_article(article_content):
+    url = "https://2uaz4gpeyh.execute-api.eu-west-2.amazonaws.com/production/support-vector-machine"
+    headers = {'Content-Type': 'application/json'}
+    article_json = {"Article":{"content":article_content}}
+    response = requests.post(url, headers=headers, json=article_json)
+    if response.status_code == 200:
+        return response.content
+    else:
+        print ('error occured during api called')
+        return None
+
+# Insert row into Dynamodb table processed_articles
+def insert_row(article_id,article_content,classification):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('results')
+    table.put_item(
+        Item={
+            'article_id' :  int(article_id),
+            'naive_bayes_classification' : str(classification, 'utf-8')
+        }
+    )
+
+def update_row(article_id,classification):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('results')
+    table.update_item(
+    Key={
+        'article_id': int(article_id),
+    },
+    UpdateExpression='SET support_vector_machine = :val1',
+    ExpressionAttributeValues={
+        ':val1': str(classification, 'utf-8')
+    }
+)
